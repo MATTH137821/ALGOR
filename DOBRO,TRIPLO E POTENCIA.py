3772ebacb3f886fb0b5748f619d6a9625e29ae2a
MATTH137821 <107654054+MATTH137821@users.noreply.github.com>
2022-08-09 18:59:12 -0300

    Create DOBRO,TRIPLO E POTENCIA.py

---
+++ DOBRO,TRIPLO E POTENCIA.py
@@ -0,0 +1,11 @@
+import math
+
+print('===============================')
+n1 = int(input('Digite um numero: '))
+d = n1 * 2
+t = n1 * 3
+r = math.sqrt(n1)
+
+
+
+print('o dobro é {} , o triplo seria  {} e a raiz quadrada {}  '.format(d,t,r))
