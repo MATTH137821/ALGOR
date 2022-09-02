9594d869f3342aa3e12efd32609574645e7616ba
MATTH137821 <107654054+MATTH137821@users.noreply.github.com>
2022-09-02 20:24:10 -0300

    Create COMPRA DAS MAÇÃs.py

---
+++ COMPRA DAS MAÇÃs.py
@@ -0,0 +1,9 @@
+
+print('SEJA BEM-vindo')
+print('para efetuarmos a COMPRA DAS MAÇãS')
+print('precisaremos a quantidade adquiridas de acordo com a promoção')
+macas = int(input('Digite a quantidade: ' ))
+if macas < 12 :
+    print( 'Sendo {} de maças, o valor a ser pago será {}! reais'.format(macas,macas * 0.30))
+else:
+    print('Sendo {} de maças, o valor a ser pago será {} reais!'.format(macas,ma
